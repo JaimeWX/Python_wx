8a1bd6a373b9d70a8524cd7601f6f18ece9512ad
Jaime <1261360793@qq.com>
2022-01-31 18:23:25 +0800

    C&R

---
+++ Algorithm/circulation_recursion/sum_circulation.py
@@ -0,0 +1,7 @@
+def sum1(arr):
+    total = 0
+    for x in arr:
+        total += x
+    return total
+
+print(sum1([1,2,3,4]))
---
+++ Algorithm/circulation_recursion/sum_recursion.py
@@ -0,0 +1,6 @@
+def sum2(arr):
+    if arr == []:
+        return 0
+    else:
+        return arr[0] + sum2(arr[1:])
+print(sum2([1,2,3,4]))
